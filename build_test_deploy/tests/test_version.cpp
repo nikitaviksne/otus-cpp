f46f98fc72c55746bc02ca50b8b5bf3bb27da141
Nikita <vixnenik@yandex.ru>
2025-11-07 10:50:47 +0300

    build on specific folder

--- build_test_deploy/tests/test_version.cpp
+++ build_test_deploy/tests/test_version.cpp
@@ -8,5 +8,5 @@ TEST(TestVersion, PositiveVersion) {
 
 int main(int nArgs, char** vArgs) {
     ::testing::InitGoogleTest(&nArgs, vArgs);
-    return RUN_ALL_TESTS();
+    return RUN_ALL_TESTS(); 
 }
