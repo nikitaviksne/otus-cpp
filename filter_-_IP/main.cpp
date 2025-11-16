2ccdcee44c5601d632d54592ab17238334d5f99f
Viksne Nikita <vixnenik@yandex.ru>
2025-11-16 22:00:18 +0300

    Done with class; can be compield, but do not check ip_filter.out. Problem with tests

--- filter_-_IP/main.cpp
+++ filter_-_IP/main.cpp
@@ -5,27 +5,14 @@
 #include "process_ip.h"
 int main()
 {		
-	int res (10);
 	std::vector<uint32_t> arrIp;
-	int ip;
-	
-	while (true)
-	{
-		uint8_t f1,f2,f3,f4;
-		//функций чтения из потока
-		std::tie(res, f1, f2, f3, f4) = readFromStream(std::cin);
-		if (res<=0) break;
-		ip = (uint32_t) f4 + (f3<<8) + (f2<<16) + (f1<<24);
-		//организуем сразу отсортированный массив по принципу бинарного поиска (или сортировка слиянием?)
-		appendItem(arrIp, ip);
-	}// while true (input)
-	
-	print_ip(arrIp); //вывод отсортированного массива
-	std::vector<uint32_t> fb = grepByFirstsByte(arrIp, 1); //фильтрация по первому байту = 1
-	print_ip(fb); //вывод в отсортированном порядке адресов с первым байтом 1
-	std::vector<uint32_t> tfb = grepByFirstsByte(arrIp, 46, 70); //фильтрация по первому байту 46 и второму байту 70
-	print_ip(tfb); //вывод в отсортированном порядке адресов с первым байтом 46 и вторым 70
-	std::vector<uint32_t> ab = grepByAnyByte(arrIp, (uint32_t) 46); //фильтрация по любому байту = 46
-	print_ip(ab); //вывод в отсортированном порядке адресов с любым байтом 46
+	IpFilter ip_filter;
+	//считывание IP из стандратного потока и формирование отсортированного массива
+	std::cin >> ip_filter;
+	//вывод отсортированного массива
+	std::cout << ip_filter.arrIP;
+	std::cout << ip_filter.grepByFirstsByte(1); //фильтрация по первому байту = 1
+	std::cout << ip_filter.grepByFirstsByte(46, 70); //фильтрация по первому байту 46 и второму байту 70
+	std::cout << ip_filter.grepByAnyByte((uint32_t) 46); //фильтрация по любому байту = 46
 	return 0; 
 }
--- filter_-_IP/process_ip.cpp
+++ filter_-_IP/process_ip.cpp
@@ -6,7 +6,10 @@
 #include <regex>
 #include "process_ip.h"
 
-std::tuple<int, uint8_t, uint8_t, uint8_t, uint8_t> readFromStream(std::istream& input)
+IpFilter::IpFilter() {};
+IpFilter::IpFilter(const std::vector<uint32_t> & vec) {arrIP = vec;};
+IpFilter::~IpFilter(){};
+std::tuple<int, uint8_t, uint8_t, uint8_t, uint8_t> IpFilter::readFromStream(std::istream& input)
 {
 	std::string inputStr;
 	std::getline(input, inputStr);
@@ -23,23 +26,23 @@ std::tuple<int, uint8_t, uint8_t, uint8_t, uint8_t> readFromStream(std::istream&
 	}
 }
 
-void appendItem(std::vector<uint32_t> &ip_pool, int item)
+void IpFilter::appendItem(int item)
 {
-	//добавляет item в ip_pool сразу в обратном лексикографическом порядке
-	int size = ip_pool.size();
+	//добавляет item в arrIP сразу в обратном лексикографическом порядке
+	int size = arrIP.size();
 		static int low; //нижняя граница поиска
 		static int up; // верхняя граница поиска
 		if (size == 0)
 		{
-			ip_pool.insert(ip_pool.begin(), item);
+			arrIP.insert(arrIP.begin(), item);
 			low = 0;
-			up = 1; //ip_pool.size();
+			up = 1; //arrIP.size();
 		}
 		else
 		{	
-			if (item <(uint32_t) ip_pool[low]) //если новый элемент меньше последнего, добавленного в массив, то рассматриваем правую часть
+			if (item <(uint32_t) arrIP[low]) //если новый элемент меньше последнего, добавленного в массив, то рассматриваем правую часть
 			{
-				up = ip_pool.size() - 1;
+				up = arrIP.size() - 1;
 			}
 			else
 			{
@@ -49,78 +52,26 @@ void appendItem(std::vector<uint32_t> &ip_pool, int item)
 			while (low <= up)
 			{
 				int index = (low+up)/2;
-				if ((uint32_t)ip_pool[index] < item) //значит надо вставлять в левую часть
+				if ((uint32_t)arrIP[index] < item) //значит надо вставлять в левую часть
 				{
 					up = index - 1;
 					continue;
 				}
-				if ((uint32_t)ip_pool[index] >= item) //значит надо вставлять в правую часть от текущего индекса
+				if ((uint32_t)arrIP[index] >= item) //значит надо вставлять в правую часть от текущего индекса
 				{
 					low = index + 1;
 					continue;
 				}
 			}//while
 			//после этого цикла, если у меня такого же item не будет найдено в массиве, то возникнет ситуация, low >= up, а значит вставлять надо на позицию up+1 = low
-			ip_pool.insert(ip_pool.begin() + low, item);
+			arrIP.insert(arrIP.begin() + low, item);
 		}//else (if (size==0) )
 }
 
-
-std::vector<uint32_t> grepByFirstByte(std::vector<uint32_t> &ip_pool, uint32_t fByte)
+std::vector<uint32_t> IpFilter::grepByAnyByte(uint32_t aByte)
 {
 	std::vector<uint32_t> ip_out;
-	//ищем где начинается первый байт равнй fByte
-	auto it = std::find_if(ip_pool.begin(), ip_pool.end(), [fByte](uint32_t ip){return (ip & (255<<24)) == (uint32_t) fByte << 24;});
-	if (it!=ip_pool.end())
-	{
-		for (auto iii=it; iii!=ip_pool.end(); ++iii)
-		{
-			//Выделяю первую октеты IP и сравниваю ее с fByte
-			if ((*iii & (255<<24)) == (uint32_t) fByte << 24)
-			{
-				ip_out.push_back(*iii);
-			}
-			else //т.к вектор отсортирован, значит дальше ничего уже быть не может
-				break;
-		}
-	}
-	return ip_out;
-}
-
-std::vector<uint32_t> grepByTwoFirstByte(std::vector<uint32_t> &ip_pool, uint32_t fByte, uint32_t sByte)
-{
-	std::vector<uint32_t> ip_out;
-	//ищем где начинается первый байт равнй fByte
-	auto it = std::find_if(ip_pool.begin(), ip_pool.end(), [fByte](uint32_t ip){return (ip & (255<<24)) == (uint32_t) fByte << 24;});
-	if (it!=ip_pool.end())
-	{
-		bool flag (0); //найден второй байт
-		for (auto iii=it; iii!=ip_pool.end(); ++iii)
-		{
-			//Выделяю первую октету IP и сравниваю ее с fByte
-			if ((*iii & (255<<24) ) == (uint32_t) (fByte << 24) )
-			{
-				if ((*iii & (255<<16)) == (uint32_t) (sByte << 16))
-				{
-					flag = 1;
-					ip_out.push_back(*iii);
-				}
-				else if (flag) //т.е уже был найден второй байт, значит дальше ничего уже не будет
-					break;
-			}
-		
-		}
-	}
-
-	return ip_out;
-}
-
-
-
-std::vector<uint32_t> grepByAnyByte(const std::vector<uint32_t> &ip_pool, uint32_t aByte)
-{
-	std::vector<uint32_t> ip_out;
-	for (auto iii=ip_pool.begin(); iii!=ip_pool.end(); ++iii)
+	for (auto iii=arrIP.begin(); iii!=arrIP.end(); ++iii)
 	{
 		bool fb ((*iii & (255<<24)) == ((uint32_t) (aByte << 24) ) ); 
 		bool sb ((*iii & (255<<16)) == ((uint32_t) (aByte << 16) ) ); 
@@ -133,7 +84,7 @@ std::vector<uint32_t> grepByAnyByte(const std::vector<uint32_t> &ip_pool, uint32
 	return ip_out;
 }
 
-void print_ip(const std::vector<uint32_t>& pool)
+void IpFilter::print_ip(const std::vector<uint32_t>& pool)
 {
 
 	for(int iii = 0; iii < pool.size(); iii++) 
@@ -141,4 +92,32 @@ void print_ip(const std::vector<uint32_t>& pool)
 		std::cout << ((pool[iii]&(255<<24))>>24) << "." << ((pool[iii]&(255<<16))>>16) <<"." << ((pool[iii]&(255<<8))>>8) << "." << ((pool[iii]&(255<<0))>>0) << "\n";
 		
 	}
+}
+
+std::ostream& operator<<(std::ostream &stream, std::vector<uint32_t> vec)
+{
+	for (auto iii=vec.begin(); iii!=vec.end(); iii++)
+	{
+		stream << *iii << "\n";
+	}
+    return stream;
+}
+
+std::istream& operator>>(std::istream& stream,  IpFilter & ip_filter )
+{
+	
+	int ip;
+	int res;	
+//	while (true)
+	{
+		uint8_t f1,f2,f3,f4;
+		//функций чтения из потока
+		std::tie(res, f2, f2, f3, f4) = ip_filter.readFromStream(stream);
+		std::cout << "Прочитали из потока: f1 = "<< (int) f1 << " f2 = " << (int) f2 << " f3 = " << (int) f3 << " f4 = " << (int) f4 << " ip = " << ip << " res = " << res << "\n";
+//		if (res<=0) break;
+		ip = (uint32_t) f4 + (f3<<8) + (f2<<16) + (f1<<24);
+		//организуем сразу отсортированный массив по принципу бинарного поиска (или сортировка слиянием?)
+		ip_filter.appendItem(ip);
+	}// while true (input)
+    return stream;
 }
