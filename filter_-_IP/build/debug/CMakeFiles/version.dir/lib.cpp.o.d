9132b538165c027e821702975bf6595966f562cc
Nikita <vixnenik@yandex.ru>
2025-11-17 09:54:13 +0300

    Done rewriting to class

---
+++ filter_-_IP/build/debug/CMakeFiles/version.dir/lib.cpp.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/version.dir/lib.cpp.o: \
+ /home/nikita/Документы/C_Cpp_progs/otus-cpp/filter_-_IP/lib.cpp \
+ /usr/include/stdc-predef.h \
+ /home/nikita/Документы/C_Cpp_progs/otus-cpp/filter_-_IP/lib.h \
+ /home/nikita/Документы/C_Cpp_progs/otus-cpp/filter_-_IP/build/debug/version.h
---
+++ filter_-_IP/build/debug/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/nikita/Документы/C_Cpp_progs/otus-cpp/googletest
+# Build directory: /home/nikita/Документы/C_Cpp_progs/otus-cpp/filter_-_IP/build/debug/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
