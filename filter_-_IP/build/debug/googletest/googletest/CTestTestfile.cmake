4fec8fe8f0b5de9160ad0c7fe87e14d96c25e9c7
Nikita <vixnenik@yandex.ru>
2025-11-17 09:54:31 +0300

    Done rewriting to class

--- filter_-_IP/build/debug/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nikita/Документы/C_Cpp_progs/otus-cpp/googletest/googletest
-# Build directory: /home/nikita/Документы/C_Cpp_progs/otus-cpp/filter_-_IP/build/debug/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
